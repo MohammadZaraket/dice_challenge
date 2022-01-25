c50b84d09da5bdbfefee009fcbc2112e96b58c3e
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-01-25 16:54:33 +0200

    Final Game

---
+++ dice.js
@@ -0,0 +1,41 @@
+window.onload = function(){
+
+var player1 = getRandom();
+var player2 = getRandom();
+var result = document.querySelector("h1");
+var image1 = document.querySelector(".img1");
+var image2 = document.querySelector(".img2");
+
+if(player1 > player2)
+{
+    result.innerHTML="Player 1 WON, Refresh ME! "
+
+}
+else if(player1 < player2)
+{
+    result.innerHTML="Player 2 WON, Refresh ME! "
+
+}
+else 
+{
+    result.innerHTML="IT is a DRAW, Refresh ME! "
+}
+
+
+
+
+
+
+
+ function getRandom()
+ {
+    return Math.floor(Math.random() * 6) + 1;
+ }
+
+
+
+ image1.src="images/dice"+player1+".png";
+ image2.src="images/dice"+player2+".png";
+
+
+};
